839fe6cd55878bad726516b03a292b40cf8ff682
Kayla Chan <kayla.chan.wy@gmail.com>
2023-07-19 11:04:38 +0100

    Updates

---
+++ precache-manifest.fd34f6895730fcc7f0665d33ff533628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01577890c4b01e0582e1",
+    "url": "/currency-converter/static/css/main.4a9132c4.chunk.css"
+  },
+  {
+    "revision": "01577890c4b01e0582e1",
+    "url": "/currency-converter/static/js/main.01577890.chunk.js"
+  },
+  {
+    "revision": "2b4e57923141e0aa228a",
+    "url": "/currency-converter/static/js/1.2b4e5792.chunk.js"
+  },
+  {
+    "revision": "83146c7f9a5a62abc4a9",
+    "url": "/currency-converter/static/js/2.83146c7f.chunk.js"
+  },
+  {
+    "revision": "e118c3c98a4cc3bb8f58",
+    "url": "/currency-converter/static/js/runtime~main.e118c3c9.js"
+  },
+  {
+    "revision": "3641f9175ed37d321e25fb5f82c6d2ac",
+    "url": "/currency-converter/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1902605108392ab264f17313935cc0f0.js"
+  "/currency-converter/precache-manifest.fd34f6895730fcc7f0665d33ff533628.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/currency-converter/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
