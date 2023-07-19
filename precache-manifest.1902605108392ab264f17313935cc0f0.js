ed0ca9354ebb327fbc37ad13fb9b66cce6d14600
Kayla Chan <kayla.chan.wy@gmail.com>
2023-07-19 10:40:47 +0100

    Updates

---
+++ precache-manifest.1902605108392ab264f17313935cc0f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01577890c4b01e0582e1",
+    "url": "/static/css/main.4a9132c4.chunk.css"
+  },
+  {
+    "revision": "01577890c4b01e0582e1",
+    "url": "/static/js/main.01577890.chunk.js"
+  },
+  {
+    "revision": "2b4e57923141e0aa228a",
+    "url": "/static/js/1.2b4e5792.chunk.js"
+  },
+  {
+    "revision": "83146c7f9a5a62abc4a9",
+    "url": "/static/js/2.83146c7f.chunk.js"
+  },
+  {
+    "revision": "7f8b219cda3dac616ce9",
+    "url": "/static/js/runtime~main.7f8b219c.js"
+  },
+  {
+    "revision": "f6b9aca24608b6a7da9dbc683afd33c6",
+    "url": "/index.html"
+  }
+];
